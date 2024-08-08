af75b07727ce6c6629191ab504f677395814b97e
Lqingzhilan <972026665@qq.com>
2024-08-08 18:28:32 +0800

    update

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -4,11 +4,19 @@ pcall(function()
 end)
 
 local options = {
-  ensure_installed = { "lua", "luadoc", "printf", "vim", "vimdoc" },
+  ensure_installed = { "c", "lua", "luadoc", "printf", "vim", "vimdoc" },
 
   highlight = {
     enable = true,
     use_languagetree = true,
+    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+    disable = function(lang, buf)
+        local max_filesize = 10 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+            return true
+        end
+    end,
   },
 
   indent = { enable = true },
